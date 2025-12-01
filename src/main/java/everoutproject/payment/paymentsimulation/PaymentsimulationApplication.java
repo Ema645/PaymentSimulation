c6c7272a0698c1e654be544540cf1773c4ac141d
Emanue Peter <emanuel23.peter@gmail.com>
2025-12-01 18:58:57 +0100

    Inital Commit

---
+++ src/main/java/everoutproject/payment/paymentsimulation/PaymentsimulationApplication.java
@@ -0,0 +1,13 @@
+package everoutproject.payment.paymentsimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsimulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/everoutproject/payment/paymentsimulation/PaymentsimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package everoutproject.payment.paymentsimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
